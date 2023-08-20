1dc689af10a91aa6dc1cb3ca9a281e6d76f4cd5e
Ausmanow <adel.usmanoff@yandex.ru>
2023-08-20 12:00:07 +0300

    converter

---
+++ main.js
@@ -0,0 +1,57 @@
+const rates = {};
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const elementGBP = document.querySelector('[data-value="GBP"]');
+
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
+const select = document.querySelector('#select');
+
+getCurrencies()
+
+setInterval(getCurrencies, 10000);
+
+async function getCurrencies () {
+    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+    const data = await response.json();
+    const result = await data;
+    console.log(result);
+
+    console.log(result.Valute.USD.Value);
+
+    rates.USD = result.Valute.USD;
+    rates.EUR = result.Valute.EUR;
+    rates.GBP = result.Valute.GBP;
+
+    console.log(rates);
+
+    elementUSD.textContent = rates.USD.Value.toFixed(2);
+    elementEUR.textContent = rates.EUR.Value.toFixed(2);
+    elementGBP.textContent = rates.GBP.Value.toFixed(2);
+
+    if (rates.USD.Value > rates.USD.Previous) {
+        elementUSD.classList.add('top');
+    } else {
+        elementUSD.classList.add('bottom');
+    }
+
+    if (rates.EUR.Value > rates.EUR.Previous) {
+        elementUSD.classList.add('top');
+    } else {
+        elementUSD.classList.add('bottom');
+    }
+
+    if (rates.GBP.Value > rates.GBP.Previous) {
+        elementUSD.classList.add('top');
+    } else {
+        elementUSD.classList.add('bottom');
+    }
+}
+
+input.onInput = convertValue;
+select.onInput = convertValue;
+
+
+function convertValue() {
+    result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
+}
