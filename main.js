2898e27f1fed08c96577e404580b88f5727a8a7f
Ausmanow <adel.usmanoff@yandex.ru>
2023-09-09 13:44:36 +0300

    fix

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ async function getCurrencies () {
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
     const data = await response.json();
     const result = await data;
-    console.log(result);
-
-    console.log(result.Valute.USD.Value);
 
     rates.USD = result.Valute.USD;
     rates.EUR = result.Valute.EUR;
@@ -48,8 +45,8 @@ async function getCurrencies () {
     }
 }
 
-input.onInput = convertValue;
-select.onInput = convertValue;
+input.oninput = convertValue;
+select.oninput = convertValue;
 
 
 function convertValue() {
